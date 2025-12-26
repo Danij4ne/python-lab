3376484d6c8ef6ffc8f9e0f847606dcad0f7f06d
Daniel Jane <danij4ne@gmail.com>
2025-12-26 11:00:54 +0100

    add sql magic commands reference

---
+++ 09_sql_and_databases/01_sqlite_basics/sql_magic.py
@@ -0,0 +1,46 @@
+ 
+# JUPYTER NOTEBOOK MAGIC COMMANDS
+ 
+# TYPES OF MAGIC COMMANDS
+ 
+# %    -> Line Magics   (apply to a single line)
+# %%   -> Cell Magics   (apply to the entire cell)
+
+
+# LINE MAGIC COMMANDS
+ 
+
+"""
+%pwd                -> Prints the current working directory
+
+%ls                 -> Lists all files in the current directory
+
+%history            -> Shows the command history
+
+%reset              -> Resets the namespace and deletes all user-defined variables
+
+%who                -> Lists all variables in the namespace
+
+%whos               -> Shows detailed information about all variables
+
+%matplotlib inline  -> Displays matplotlib plots inside the notebook
+
+%timeit             -> Measures the execution time of a single statement
+
+%lsmagic            -> Shows all available magic commands
+"""
+
+
+ 
+# CELL MAGIC COMMANDS
+ 
+
+"""
+%%writefile         -> Creates or overwrites a file with the contents of the entire cell
+
+%%HTML              -> Allows writing HTML code inside the cell
+
+%%javascript        -> Allows writing JavaScript code inside the cell
+
+%%bash              -> Allows running Bash commands inside the cell
+"""
